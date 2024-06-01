e3bf5aa91f84bbae525a8d64cb865f880d66d49a
Oke Marvellous <okeseyi5@gmail.com>
2024-06-01 23:38:41 +0100

    test

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,22 +6,14 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:iamArvy/Amos_Transport.git master:gh-pages
-
-# # if you are deploying to https://<USERNAME>.github.io/<REPO> testing
-#ssh-keygen -t ed25519 -C "okeseyi5@gmail.com"
-# /f/Web Development/works/Serious Stuffs/Amos_Transport/.ssh/id_ed25519
-# git push -f git@github.com:iamArvy/portfolio.git master:gh-pages
+# eval $(ssh-agent)
+# $ ssh-add ~/.ssh/transport_key
+git push -f git@github.com:iamArvy/amostransports.git master:gh-pages
 
 cd -
