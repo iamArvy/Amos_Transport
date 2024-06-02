933d90acb7674a1f150737077aac1a53a343f39e
Oke Marvellous <okeseyi5@gmail.com>
2024-06-02 02:28:13 +0100

    er

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import routes from './routes';
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 const router = createRouter({
-  history: createWebHistory('/your-github-repo/'),
+  history: createWebHistory('/amostransports/'),
   // base: process.env.NODE_ENV === 'production' ? '/amostransports/' : '/',
     routes,
   });
